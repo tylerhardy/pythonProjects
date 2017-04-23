b94c7937553024af03a9cd013eb1a69ce2893e5b
tylerhardy <tylerhardy@gmail.com>
2017-04-23 09:21:45 -0600

    Modified randomQuizGenerator.py

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -39,10 +39,18 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 
 # Generate 35 quiz files.
 for quizNum in range(35):
-    # TODO: Create the quiz and answer key files.
+    # Create the quiz and answer key files.
+    quizFile = open('capitalsquiz%s.txt' %(quizNum + 1), 'w')
+    answerKeyFil = open('capitalsquiz_answers%s.txt' %(quizNum + 1), 'w')
 
-    # TODO: Write out the header for the quiz.
+    # Write out the header for the quiz.
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((' '*20) + 'State Capitals Quiz (Form %s)' %(quizNum + 1))
+    quizFile.write('\n\n')
 
-    # TODO: Shuffle the order of the states.
+    # Shuffle the order of the states.
+    states = list(capitals.keys())
+    random.shuffle(states)
 
-    # TODO: Loop through all 50 states, making a question for each.
+    # Loop through all 50 states, making a question for each.
+    
